a4586b68693d6304d2ad6130eabd1e34519881a5
Euan Kemp <euank@euank.com>
2018-08-29 17:05:57 -0700

    Add an example

---
+++ examples/git_pathspec.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+extern crate include_repo;
+
+use std::io::Write;
+
+// A pathspec may be used to include only a subset of the git repo's files
+include_repo!(REPO_CARGO_TOML_TAR, "Cargo.toml");
+
+fn main() {
+	let mut cmd = std::process::Command::new("tar");
+	cmd.arg("t")
+	   .stdin(std::process::Stdio::piped())
+	   .stdout(std::process::Stdio::piped());
+
+	let mut child = cmd.spawn().unwrap();
+	{
+		let stdin = child.stdin.as_mut().unwrap();
+		stdin.write_all(&REPO_CARGO_TOML_TAR[..]).unwrap();
+	}
+	let output = child.wait_with_output().unwrap();
+	assert_eq!(String::from_utf8_lossy(&output.stdout).trim(), "Cargo.toml");
+}
