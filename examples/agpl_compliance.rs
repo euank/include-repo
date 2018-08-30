2690e553c05c4410fcbae940ce640ecbe3fce9a1
Euan Kemp <euank@euank.com>
2018-08-29 17:26:13 -0700

    example: make the agpl_compliance example simpler
    This makes it more reasonable to run under travis-ci.

--- examples/agpl_compliance.rs
+++ examples/agpl_compliance.rs
@@ -9,17 +9,10 @@ use std::io::Write;
 include_repo!(SOURCE_CODE);
 
 fn main() {
-	match std::env::args().skip(1).next() {
-		Some(ref s) if s == "agpl" => {
-			let mut f = std::fs::File::create("/tmp/code.tar").expect("could not open file");
-			f.write_all(&SOURCE_CODE[..]).expect("could not write file");
-			println!("The source code of this program has been written to {} for agpl compliance reasons", "/tmp/code.tar");
-		},
-		None => {
-			println!("Hello world\nPlease pass 'agpl' as an argument to get my source code!");
-		}
-		_ => {
-			println!("Unrecognized argument, I only know how to take 'agpl' as an argument to write out my code");
-		}
-	};
+    let mut f = std::fs::File::create("/tmp/code.tar").expect("could not open file");
+    f.write_all(&SOURCE_CODE[..]).expect("could not write file");
+    println!(
+        "The source code of this program has been written to {} for agpl compliance reasons",
+        "/tmp/code.tar"
+    );
 }
